ec72d5cc7726c600015181e0a1f3fffb0cfdd8b6
Scott Rouse <scottwrouse@gmail.com>
2011-11-16 16:07:39 -0700

    search form styling/themeing

--- template.php
+++ template.php
@@ -140,3 +140,25 @@ function neon_preprocess_block(&$variables, $hook) {
   $variables['classes_array'][] = 'count-' . $variables['block_id'];
 }
 // */
+
+
+/**
+  * Implementation of hook_form_alter().
+  *
+  * Changes default value of Search field and search button and
+  * set size of search field.
+  */
+
+function neon_form_alter(&$form, &$form_state, $form_id) {
+  if ($form_id == 'search_block_form') {
+    $form['search_block_form']['#title'] = t('Search'); // Change the text on the label element
+    $form['search_block_form']['#title_display'] = 'invisible'; // Toggle label visibilty
+    $form['search_block_form']['#size'] = 30;  // define size of the textfield
+    $form['search_block_form']['#default_value'] = t('Search...'); // Set a default value for the textfield
+    $form['actions']['submit']['#value'] = t('Go'); // Change the text on the submit button
+
+// Add extra attributes to the text box
+    $form['search_block_form']['#attributes']['onblur'] = "if (this.value == '') {this.value = 'Search...';}";
+    $form['search_block_form']['#attributes']['onfocus'] = "if (this.value == 'Search...') {this.value = '';}";
+  }
+}
